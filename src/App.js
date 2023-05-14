dd44ec6337c4b5237e53137caec86f8c6ef2e047
Krish <krishparekh9@gmail.com>
2023-05-14 16:14:57 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSourceEventLink, useDestinationEventLink } from "@event-inc/link";
 
 function App() {
+  const { open, close } = useSourceEventLink({
+    group: "krish-parekh-01",
+    linkTokenEndpoint: "https://some-link-token-endpoint.com",
+    baseUrl:
+      "https://event-link-frontend-git-development-buildable.vercel.app/",
+  });
+
+  const { open: open2, close: close2 } = useDestinationEventLink({
+    group: "krish-parekh-02",
+    linkTokenEndpoint: "https://some-link-token-endpoint.com",
+    baseUrl:
+      "https://event-link-frontend-git-development-buildable.vercel.app/",
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={open}>
+          Click here to see the magic on the sources
+        </button>
+        <button onClick={open2}>
+          Click here to see the magic on the destinations
+        </button>
       </header>
     </div>
   );
