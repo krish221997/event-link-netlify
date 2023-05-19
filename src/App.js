fabcc225876eb7e04138e6025721131e638291ba
Krish <krishparekh9@gmail.com>
2023-05-20 02:58:09 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useSourceEventLink, useDestinationEventLink } from "@event-inc/link";
 
 function App() {
-  const { open } = useSourceEventLink({
-    onSuccess: (data) => {
-      console.log(data);
+  const { open, close } = useSourceEventLink({
+    group: "sandbox-demo-9",
+    linkTokenEndpoint:
+      "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
+    environment: "sandbox",
+    onSuccess(source) {
+      console.log("Source", source);
+      setTimeout(() => {
+        console.log("Closing");
+        close();
+      }
+      , 5000);
     },
-    
-
-    group: "sandbox demo",
-    linkTokenEndpoint: "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
-    environment: "sandbox"
+    onError(error) {
+      console.log("Error", error);
+    },
+    onClose: () => {
+      console.log("closed");
+    },
   });
 
   const { open: open2 } = useDestinationEventLink({
-    group: "sandbox demo 2",
-    linkTokenEndpoint: "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
-    "environment": "sandbox"
+    group: "sandbox-demo-10",
+    linkTokenEndpoint:
+      "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
+    environment: "sandbox",
+    onSuccess(source) {
+      console.log("Destination", source);
+      setTimeout(() => {
+        console.log("Closing");
+        close();
+      }
+      , 5000);
+    },
+    onError(error) {
+      console.log("Error", error);
+    },
+    onClose: () => {
+      console.log("closed");
+    },
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <button onClick={open}>
-          Click here to see the magic on the sources
-        </button>
-        <button onClick={open2}>
-          Click here to see the magic on the destinations
-        </button>
-      </header>
+    <div>
+      <button onClick={open}>Click here to see the magic on the sources</button>
+      <button onClick={open2}>
+        Click here to see the magic on the destinations
+      </button>
     </div>
   );
 }
