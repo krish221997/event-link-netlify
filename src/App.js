6bf200d5e3e9ab62c918800cbaf93dbac41f4fe5
Krish <krishparekh9@gmail.com>
2023-09-01 22:59:24 +0530

    Connections

--- src/App.js
+++ src/App.js
@@ -1,54 +1,88 @@
-import { useSource, useDestination } from "@event-inc/link";
+import { useSource, useDestination, useLink } from "@event-inc/link";
 
 function App() {
-  const { open, close } = useSource({
-    title: "Hello World",
-    group: "sandbox-demo-9",
-    linkTokenEndpoint:
-      "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
-    environment: "sandbox",
-    onSuccess(source) {
-      console.log("Source", source);
-      setTimeout(() => {
-        console.log("Closing");
-        close();
-      }
-      , 5000);
-    },
-    onError(error) {
-      console.log("Error", error);
-    },
-    onClose: () => {
-      console.log("closed");
-    },
-  });
 
-  const { open: open2 } = useDestination({
-    group: "sandbox-demo-10",
-    linkTokenEndpoint:
-      "https://awaresillycareware--krishparekh11.repl.co/create-link-token",
-    environment: "sandbox",
-    onSuccess(source) {
-      console.log("Destination", source);
-      setTimeout(() => {
-        console.log("Closing");
-        close();
-      }
-      , 5000);
+  // 
+  // const { open } = useOauthConnection){
+  //   keys: [
+  //     "key"
+  //   ]
+  // }
+
+  // const { open, close } = useSource({
+  //   baseUrl:'http://localhost:3003',
+  //   linkHeaders: {
+  //     'ngrok-skip-browser-warning':"any"
+  //   },
+  //   title: "Hello World",
+  //   group: "sandbox-demo-9",
+  //   linkTokenEndpoint:
+  //     "http://localhost:4000/create-link-token",
+  //   // environment: "sandbox",
+  //   onSuccess(source) {
+  //     console.log("Source", source);
+  //     setTimeout(() => {
+  //       console.log("Closing");
+  //       close();
+  //     }
+  //     , 5000);
+  //   },
+  //   onError(error) {
+  //     console.log("Error", error);
+  //   },
+  //   onClose: () => {
+  //     console.log("closed");
+  //   },
+  // });
+
+  // const { open: open2 } = useDestination({
+  //   baseUrl:'http://localhost:3003',
+  //   group: "sandbox-demo-10",
+  //   linkHeaders: {
+  //     'ngrok-skip-browser-warning':"any"
+  //   },
+  //   linkTokenEndpoint:
+  //     "http://localhost:4000/create-link-token",
+  //   // environment: "sandbox",
+  //   onSuccess(source) {
+  //     console.log("Destination", source);
+  //     setTimeout(() => {
+  //       console.log("Closing");
+  //       close();
+  //     }
+  //     , 5000);
+  //   },
+  //   onError(error) {
+  //     console.log("Error", error);
+  //   },
+  //   onClose: () => {
+  //     console.log("closed");
+  //   },
+  // });
+
+  const { open: open3 } = useLink({
+    title: "Connections",
+    // linkTokenEndpoint: "http://localhost:4000/create-link-token",
+    linkTokenEndpoint: "https://ultimatetrustingreality.krishparekh11.repl.co/create-link-token",
+    // baseUrl: 'http://localhost:3003',
+    baseUrl: 'https://development-link.event.dev',
+    onSuccess: (integrations) => {
+      console.log(integrations, "<======= integrations");
     },
-    onError(error) {
-      console.log("Error", error);
+    onError: (error) => {
+      console.log(error, "<===== error")
     },
-    onClose: () => {
-      console.log("closed");
-    },
+
   });
 
   return (
     <div>
-      <button onClick={open}>Click here to see the magic on the sources</button>
-      <button onClick={open2}>
+      {/* <button onClick={open}>Click here to see the magic on the sources</button> */}
+      {/* <button onClick={open2}>
         Click here to see the magic on the destinations
+      </button> */}
+      <button onClick={open3}>
+        Click here to see the power of Connections
       </button>
     </div>
   );
